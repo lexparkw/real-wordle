439950d850331126757cf198f17b2d3bbb55c694
Alex Park <alexandr.pak@xeneta.com>
2023-07-17 17:12:10 +0200

    adds keyboard event listener

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('HETY')
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
+
+async function init() {
+
+    document.addEventListener("keydown", (event) => {
+        const action = event.key;
+
+        if (action == 'Enter') {
+            commit()
+        } else if (action == 'Backspace') {
+            backspace()
+        } else if (isLetter(action)) {
+            addLeter(action)
+        }
+    })
+}
+
+init()
